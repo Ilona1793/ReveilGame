abe3a602af7e7df158194fa4957c7dbcc68d436b
mayliieh <74548688+mayliieh@users.noreply.github.com>
2020-11-18 12:30:17 +0100

    Blätter_timing
    Level 2 Animation Timing, aber nicht sichtbar beim Spielen

---
+++ Reveil/Assets/Scripts/StartAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour
+{
+    Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Level 2
+        if (Time.timeSinceLevelLoad >= 30)
+        {
+            m_Animator.Play("AhornAnimation");
+        } else
+        {
+            m_Animator.Play("Stop");
+        }
+
+
+        //Level 3
+        if (Time.timeSinceLevelLoad >= 5)
+        {
+            m_Animator.Play("Vogel");
+        }
+        else
+        {
+            m_Animator.Play("StopVogel");
+        }
+    }
+}
