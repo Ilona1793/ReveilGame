c28bd06d388a04792411d39fd306eb62dfa1511d
Juliane98 <74611516+Juliane98@users.noreply.github.com>
2020-11-18 17:36:21 +0100

    UI

---
+++ Reveil/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    // Start Game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Adjust Volume
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+}
