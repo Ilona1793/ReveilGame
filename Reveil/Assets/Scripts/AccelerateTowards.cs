9f4746af197699738e40fa63add722719454a598
Ilona1793 <ilona.schleindl@web.de>
2020-12-09 17:30:46 +0100

    StardustVersuch

---
+++ Reveil/Assets/Scripts/AccelerateTowards.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateTowards : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform transTowards;
+
+    [SerializeField]
+    float fSpeed;
+
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        if (transTowards == null)
+        {
+            transTowards = FindObjectOfType<Sphere>().transform;
+        }
+    }
+
+
+    void Update()
+    {
+        rigid.velocity += (transTowards.position - transform.position).normalized * fSpeed * Time.deltaTime;
+    }
+}
---
+++ Reveil/Assets/Scripts/Stardust.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stardust : MonoBehaviour
+{
+    private ParticleSystem particle;
+    private SpriteRenderer sr;
+    private CircleCollider2D bc;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        particle = GetComponentInChildren<ParticleSystem>();
+        sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnColliderEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Break());
+        }
+    }
+
+    private IEnumerator Break()
+    {
+        particle.Play();
+
+        sr.enabled = false;
+        bc.enabled = false;
+
+        yield return new WaitForSeconds(particle.main.startLifetime.constantMax);
+        Destroy(gameObject);
+    }
+}
