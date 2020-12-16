5bd6e280eedfc9e397e86c1964c6f0a115377ca8
Juliane98 <74611516+Juliane98@users.noreply.github.com>
2020-12-16 11:41:47 +0100

    Level 01 Animation Gleiter Problem

---
+++ Reveil/Assets/Scripts/CheckpointGleiter01.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointGleiter01 : MonoBehaviour
+{
+    public bool gleiterAnimation;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gleiterAnimation = true;
+        }
+    }
+}
